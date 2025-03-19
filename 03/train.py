a6892e01b3c9e3f99b4cfebca1087259fe68138b
klxcoder <klxcoder@gmail.com>
2025-03-20 04:22:59 +0700

    print first 100 characters

--- 03/train.py
+++ 03/train.py
@@ -2,3 +2,6 @@ with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 print("length of dataset in characters: ", len(text)) # 1115394
+
+# let's look at the first 100 characters
+print(text[:100])
